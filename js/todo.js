4516897581a6c8a084201843c1b2bd0447022e3a
nvnvenki <nvnvenki@gmail.com>
2014-01-19 13:30:58 +0530

    1)Added angular.js library 2) Added complete functionality of adding and removing tasks 3) Added few css effects

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,49 @@
-(function(){
-	console.log("Started")
+console.log('Started')
+var id = 0
 
-})()
+function todoController($scope){
+	console.log("In todoController")
+
+	$scope.todos = []
+	
+
+	$scope.totalTodos = $scope.todos.length
+
+	$scope.showForm = "hide"
+
+	$scope.removeTask = function(id){
+		$scope.todos = $scope.todos.filter(function(todo){
+			return todo.id != id
+		})
+	}
+
+	$scope.getTotalTodos = function(){
+		return $scope.todos.length
+	}
+
+	$scope.isEmpty = function(){
+		return $scope.todos.length == 0 ? "show" : "hide"
+	}
+
+	$scope.hasToDo = function(){
+
+		return $scope.todos.length > 0 ? "show" : "hide"
+	}
+
+	$scope.displayForm = function(){
+		$scope.showForm = "show"
+	}
+
+	$scope.addTask = function(){
+		// console.log($scope.time)
+		if($scope.task && $scope.time)
+		{
+			
+			$scope.todos.push({task : $scope.task, time : $scope.time, done : false, id : id++ })
+			$scope.showForm = "hide"
+			$scope.task = ""
+			$scope.time = ""
+		}
+	}
+}
+	
